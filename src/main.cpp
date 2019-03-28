fab7fde98bd9d596b6be2192554d97847b02581d
KerouichaReda <kerouicha.reda@gmail.com>
2019-03-28 15:20:23 +0800

    sha256 header

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,15 @@
-#include"Header_Files/Blockchain.hpp"
-#include<iostream>
+#include <iostream>
+#include "Header_Files/sha256.hpp"
 
+using std::string;
+using std::cout;
+using std::endl;
 
-
-int main()
+int main(int argc, char *argv[])
 {
+    string input = "grape";
+    string output1 = sha256(input);
 
-	std::cout << "Test" << std::endl;
-	std::vector<std::string> mydata;
-	mydata.push_back("9d6535e9ad3fa7cc87f24b4e7e18f76cbce4d9cc0aa46418aada5338196630cb");
-	mydata.push_back("dd71cb1439378cc340bc464c87d3c382167efcec8420f78f1e72726fa5b47db3 ");
-	mydata.push_back("2a8cd536308771fa93593a84ef1f92de821c782ea099e251a14afc93efa892f0 ");
-	
-	Block MyBlock = Block(563849, "00000000000000000001fbe60c55caac208cd9d1d5e318f8fe3b732c8fafc247", "0000000000000000000bb03a224520c5ef7d3270d06c0aff09f39a003033e842", "2596959192", mydata);
-	MyBlock.displayblock();
-	//std::string header = std::to_string(index) + prevHash + nonce;
-	
-
-	////
-	//Blockchain bc(0);
-	//std::cout << bc.getNumBlocks() << std::endl;
-	//bc.getBlock(0).displayblock();
-	//std::vector<std::string> merkel;
-	//std::string previousHash = bc.getBlock(0).getHash();
-	//std::pair<std::string, std::string> p = Common::findHash(1, previousHash, mydata);
-	//bc.addBlock(1, previousHash, p.first, p.second, merkel);
-
-	//std::cout << bc.getNumBlocks() << std::endl;
-	//bc.getBlock(1).displayblock();
-	getchar();
-
-	return 0;
+    cout << "sha256('"<< input << "'):" << output1 << endl;
+    return 0;
 }
